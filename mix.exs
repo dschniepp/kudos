97231bd1bcb4ade2ae7f470a23560e17f0f5c936
Daniel Schniepp <d.schniepp@indale.com>
2021-05-24 20:19:08 +0200

    support elixir 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kudos.Mixfile do
     [
       app: :kudos,
       version: "0.2.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env == :prod,
       description: description(),
       package: package(),
