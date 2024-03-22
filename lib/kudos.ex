eae19f4b2ed4155a1ad201044190d236ff001fdf
Raul Zavaczki <raul.zavaczki@ni.com>
2024-03-22 15:29:10 +0200

    add

--- lib/kudos.ex
+++ lib/kudos.ex
@@ -99,7 +99,7 @@ defmodule Kudos do
   end
 
   defp load_deps_meta_data(include_dev_deps) do
-    Mix.Dep.load_on_environment([])
+    Mix.Dep.load_and_cache()
     |> Enum.map(fn dep ->
       Mix.Dep.in_dependency(dep, fn _ ->
         case is_umbrella?(dep) do
